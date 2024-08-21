3cf92773758a6aed6e9f9d670fa0f94e8f0b035f
DYusu <abadilla2009@gmail.com>
2024-08-21 19:50:34 -0400

    Ajustando DB

--- arrendahogar_aplicacion/admin.py
+++ arrendahogar_aplicacion/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from .models import Usuarios, Propiedades, Arrendamientos, Comunas, Regiones, PropiedadesComunas
 
-@admin.register(Usuarios)
-class UsuariosAdmin(admin.ModelAdmin):
-    list_display = ('nombres', 'apellidos', 'rut', 'direccion', 'telefono_personal', 'correo_electronico', 'tipo_usuario', 'fecha_registro')
-    search_fields = ('nombres', 'apellidos', 'rut', 'correo_electronico')
-
 @admin.register(Propiedades)
 class PropiedadesAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'descripcion', 'm2_construidos', 'm2_totales', 'cantidad_estacionamientos', 'cantidad_habitaciones', 'cantidad_banos', 'direccion', 'tipo_inmueble', 'precio_mensual', 'arrendador', 'comuna')
--- arrendahogar_aplicacion/models.py
+++ arrendahogar_aplicacion/models.py
@@ -2,6 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+        
 class Usuarios(models.Model):
     nombres = models.CharField(max_length=255)
     apellidos = models.CharField(max_length=255)
@@ -9,12 +25,39 @@ class Usuarios(models.Model):
     direccion = models.CharField(max_length=255)
     telefono_personal = models.CharField(max_length=20, blank=True, null=True)
     correo_electronico = models.CharField(unique=True, max_length=255)
-    tipo_usuario = models.CharField(max_length=20)
     fecha_registro = models.DateTimeField(blank=True, null=True)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING, blank=True, null=True)
+    tu = models.ForeignKey('TipoUsuario', models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'usuarios'
+
+
+class TipoUsuario(models.Model):
+    tu_id = models.AutoField(primary_key=True)
+    tu_nombre = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'tipo_usuario'
+
+
+class Regiones(models.Model):
+    nombre = models.CharField(unique=True, max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'regiones'
+
+
+class PropiedadesComunas(models.Model):
+    propiedad = models.ForeignKey('Propiedades', models.DO_NOTHING)
+    comuna = models.ForeignKey('Comunas', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'propiedades_comunas'
 
 
 class Propiedades(models.Model):
@@ -36,6 +79,15 @@ class Propiedades(models.Model):
         db_table = 'propiedades'
 
 
+class Comunas(models.Model):
+    nombre = models.CharField(unique=True, max_length=255)
+    region = models.ForeignKey(Regiones, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'comunas'
+
+
 class Arrendamientos(models.Model):
     arrendatario = models.ForeignKey(Usuarios, models.DO_NOTHING)
     propiedad = models.ForeignKey(Propiedades, models.DO_NOTHING)
@@ -45,29 +97,3 @@ class Arrendamientos(models.Model):
     class Meta:
         managed = False
         db_table = 'arrendamientos'
-
-
-class Comunas(models.Model):
-    nombre = models.CharField(unique=True, max_length=255)
-    region = models.ForeignKey('Regiones', models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'comunas'
-
-
-class Regiones(models.Model):
-    nombre = models.CharField(unique=True, max_length=255)
-
-    class Meta:
-        managed = False
-        db_table = 'regiones'
-
-
-class PropiedadesComunas(models.Model):
-    propiedad = models.ForeignKey(Propiedades, models.DO_NOTHING)
-    comuna = models.ForeignKey(Comunas, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'propiedades_comunas'
